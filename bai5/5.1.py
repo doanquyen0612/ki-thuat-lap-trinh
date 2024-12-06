7d2d9f97b96bbd53e97983672edcf21c8967f7f1
doanquyen0612 <qdoan4805@gmail.com>
2024-12-06 12:53:02 +0700

    Create 5.1.py

---
+++ bai5/5.1.py
@@ -0,0 +1,12 @@
+print ("sinh vien: Doan Van Quyen MSV:235752021610047")
+
+import mymath 
+values = [2,4,6,8,10]
+print('Squares:')
+for v in values:
+ print(mymath.square(v))
+print('Cubes:')
+for v in values:
+ print(mymath.cube(v))
+print('Average: ' + str(mymath.average(values)))
+
